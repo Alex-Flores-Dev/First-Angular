fc8f6bab85964dbe39678fcef13ede33a7b56859
Alex Flores <alex_fv92@hotmail.com>
2019-02-26 15:37:41 -0930

    Primer componente

---
+++ src/app/componente1/componente1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  template: '<h1> Mis Componentes</h1>',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
